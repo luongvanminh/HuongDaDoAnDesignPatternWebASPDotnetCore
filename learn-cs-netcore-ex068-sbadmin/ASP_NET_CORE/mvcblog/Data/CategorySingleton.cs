1477ea57c50f45507dd745d5cf90f811c0efdbd2
Minh Luong Van <luongvanminh@huflit.edu.vn>
2021-09-24 22:54:17 +0700

    Them Singleton

---
+++ learn-cs-netcore-ex068-sbadmin/ASP_NET_CORE/mvcblog/Data/CategorySingleton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using mvcblog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace mvcblog.Data
+{
+    public sealed class CategorySingleton
+    {
+        public static CategorySingleton Instance { get; } = new CategorySingleton();
+        public List<Category> listCatgegory { get; } = new List<Category>();
+
+        private CategorySingleton() { }
+
+        // only One time
+        public void Init(AppDbContext context)
+        {
+
+            if (listCatgegory.Count == 0)
+            {
+                var categories = context.Categories
+                    .Include(c => c.CategoryChildren)
+                    .AsEnumerable()
+                    .Where(c => c.ParentCategory == null)
+                    .ToList();
+
+                foreach (var item in categories)
+                {
+                    listCatgegory.Add(item);
+                }
+            }
+        }
+
+        public void Update(AppDbContext context)
+        {
+            listCatgegory.Clear();
+            Init(context);
+        }
+    }
+}
